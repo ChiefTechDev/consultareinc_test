b4e549633649708901b0a21e4c6ffe63ad6b26aa
CryptoNinja1210 <jerome.pangilinan914@gmail.com>
2024-11-01 09:26:29 +0800

    Add: vercel.json

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,http://consultareinc-test.42web.io/',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
